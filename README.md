b9f46872a338dfb4f79393795805dce3242d4a40
GraxCode <grax@graxco.de>
2018-01-03 15:21:02 +0100

    add jarprotector extractor

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-### What is this project about?
+## What is this project about?
 The ReverseCrypter can extract jar archives crypted by various java-crypters. The extractors (and their keys!) are hard-coded and may not work for newer versions.
 
-### Supported Crypters
+## Supported Crypters
 | Crypter | Extractor |
 | --- | --- |
 | CoreProject by ??? | CoreProtectEx |
 | JCrypt 1.5 by redpois0n | JCryptEx |
+| JarProtector 1.1.1 by Burn3diC3 | JarProtectorEx |
 
-### CLI
+## CLI
 | Argument | Description |
 | --- | --- |
 | --help | Displays help |
@@ -15,5 +16,8 @@ The ReverseCrypter can extract jar archives crypted by various java-crypters. Th
 | --output | Specify output file |
 | --extractor | Specify extractor |
 
-### Libraries needed
+## Libraries needed
 commons-io 2.6, commons-cli 1.4
+
+## License
+ReverseCrypt is licensed under the GNU General Public License
---
+++ src/me/nov/reversecrypt/extractors/JarProtectorEx.java
@@ -0,0 +1,71 @@
+package me.nov.reversecrypt.extractors;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.zip.GZIPInputStream;
+
+import sun.misc.BASE64Decoder;
+
+import org.apache.commons.io.IOUtils;
+
+import me.nov.reversecrypt.Extractor;
+
+public class JarProtectorEx extends Extractor {
+
+  private String configPath = "c.dat";
+
+  public JarProtectorEx(JarFile file) {
+    super(file);
+  }
+
+  public JarProtectorEx(JarFile file, String config) {
+    super(file);
+    this.configPath = config;
+  }
+
+  @Override
+  public byte[] extract() {
+    try {
+      String[] config = new String(getConfig(), "UTF-8").split("\n");
+      String path = config[1];
+      boolean compressed = Boolean.parseBoolean(config[4]);
+      String key = config[5];
+      JarEntry e = file.getJarEntry(path);
+      if(compressed) {
+        //TODO test for compressed
+        return decrypt(IOUtils.toByteArray(new GZIPInputStream(file.getInputStream(e))), key.getBytes());
+      }
+      return decrypt(IOUtils.toByteArray(file.getInputStream(e)), key.getBytes());
+    } catch (Exception ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+
+  private byte[] decrypt(byte[] enc, byte[] keybytes) {
+    byte[] decrypted = (byte[])enc.clone();
+    int i = 0;
+
+    for(int keyIndex = 0; i < decrypted.length; ++i) {
+       decrypted[i] ^= keybytes[keyIndex++];
+       if(keyIndex == keybytes.length) {
+          keyIndex = 0;
+       }
+    }
+    return decrypted;
+ }
+
+  private byte[] getConfig() throws IOException {
+    InputStream bytes = file.getInputStream(file.getJarEntry(configPath));
+    byte[] configBytes = (new BASE64Decoder()).decodeBuffer(bytes);
+
+    for (int i = 0; i < configBytes.length; ++i) {
+      configBytes[i] ^= -35;
+    }
+
+    return configBytes;
+  }
+}
